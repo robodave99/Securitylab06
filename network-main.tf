378159ef854c1ca45360f0811aa543872d6018cb
Oladapo Dele-David <dapoiyiowo@gmail.com>
2024-03-12 17:33:51 -0400

    Updated main file

--- network-main.tf
+++ network-main.tf
@@ -1,21 +1,16 @@
-#creating the iam role
+# creating the IAM role
 resource "aws_iam_role" "my_role" {
   name               = var.role_name
-  assume_role_policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Principal": {
-          "Service": "ec2.amazonaws.com"
-        },
-        "Action": "sts:AssumeRole"
-      }
-    ]
-  }
-  EOF
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17",
+    Statement = [{
+      Effect    = "Allow",
+      Principal = { Service = "ec2.amazonaws.com" },
+      Action    = "sts:AssumeRole"
+    }]
+  })
 }
+
 
 #defining the iam policy
 resource "aws_iam_policy" "my_policy" {
@@ -28,6 +23,25 @@ resource "aws_iam_policy" "my_policy" {
 resource "aws_iam_role_policy_attachment" "attach_policy" {
   role       = aws_iam_role.my_role.name
   policy_arn = aws_iam_policy.my_policy.arn
+}
+
+# Creating an iam instance profile
+resource "aws_iam_instance_profile" "my_instance_profile" {
+  name = "my_instance_profile"
+
+  role = aws_iam_role.my_role.name
+}
+
+# creating the EC2 instance with the IAM role
+resource "aws_instance" "my_instance" {
+  ami           = "ami-01387af90a62e3c01"
+  instance_type = "t2.micro"
+
+  iam_instance_profile = aws_iam_instance_profile.my_instance_profile.name
+
+  tags = {
+    Name = "MyEC2Instance"
+  }
 }
 
 #detaching the iam policy from the role
